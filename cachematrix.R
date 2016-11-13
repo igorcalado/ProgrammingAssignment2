2895af726d5842b23c5f6c467eb6bc6f1f9b6af5
igorcalado <igoracalado@gmail.com>
2016-11-13 20:52:46 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take a matrix and create a special object out of it that contains both
+## the original matrix and a cached value, and assigns the inverse of the matrix to the cached value,
+## so it can retrieve the inverse of the matrix without having to calculate it from scratch,
+## if it has already been calculated and stored.
 
-## Write a short comment describing this function
+
+## This function takes a matrix as an argument and creates a special object that contains the original
+## matrix and a cached value where the inverse matrix will be stored by the next function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function takes the special object created by the previous functions and calculates the inverse
+## of the matrix contained in that object; then, it stores the inverse of the matrix inside the cache.
+## If the inverse has already been calculated once, it only retrieves the cache.
+## When the inverse is being calculated for the first time for a given object, it prints the message
+## "calculating" before returning the result. If the inverse matrix has already been cached for
+## that object, it prints "getting cached data" and returns the cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  message("calculating")
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
